a754aaf6d0cfb1ac10c85a776af97904f6645acd
jlmunozpb <jlmunozpb@gmail.com>
2025-10-21 14:16:56 -0500

    modules

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 // import logo from './logo.svg';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Login } from './pages/Login';
-import { Register } from './pages/Register';
-import { Home } from './pages/Home';
-import { PasswordRecover } from './pages/PasswordRecover';
-import { Header } from './components/Header';
-
 import { useEffect, useReducer } from 'react';
-import { AuthContext } from './auth/authContext.js';
-import { authReducer } from './auth/authReducer.js';
-import { AppRouter } from './routers/AppRouter';
+import { AuthContext } from './services/auth/authContext.js';
+import { authReducer } from './services/auth/authReducer.js';
+import { AppRouter } from './routers/AppRouter.js';
 import './App.scss';
 
 const init = () => { return JSON.parse(sessionStorage.getItem('user') ) || { logged: false}; }
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import '../assets/styles/scss/components/Header.scss';
-
-export const Header = () => {
-  return (
-    <div className="header"><h1>Organize<span className='u-letter'>U</span></h1></div>
-  )
-}
-
-export default Header;
